5b8ddc9cb7e2f8c1cd2263f21d4f314d8fb3ce3a
ghader <gharibyan.ghader@gmail.com>
2021-10-29 14:35:17 +0330

    account controller

---
+++ src/Agahey.Web.Mvc/Controllers/ManageControllercs.cs
@@ -0,0 +1,13 @@
+﻿using Agahey.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agahey.Web.Controllers
+{
+    public class ManageController : AgaheyControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
